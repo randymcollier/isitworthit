298dc5077a71ed2e1d8a3d2d393c671a76f77ce5
Randy Collier <>
2019-02-04 23:07:42 -0600

    add versioning and tests for the api. resolves #4

--- IsItWorthIt.Api/Startup.cs
+++ IsItWorthIt.Api/Startup.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IsItWorthIt.Domain.Contracts;
+using IsItWorthIt.Domain.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +28,8 @@ namespace IsItWorthIt.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddTransient<ITellVersions, VersionService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
